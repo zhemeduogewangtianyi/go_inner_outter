7ed7cbaf942435753e766b8048225b3efc34ec9d
wb-wty514478 <wb-wty514478@alibaba-inc.com>
2021-03-03 17:29:08 +0800

    循环和判断（loop and condition）

---
+++ go_learning/src/ch5/condition/condition_test.go
@@ -0,0 +1,21 @@
+package condition
+
+import "testing"
+
+func TestIfMultiSec(t *testing.T) {
+
+	if a := 1 == 1; a {
+		t.Log("1 == 1")
+	}
+
+	/**
+	后面高端
+	*/
+
+	//if v,err := someFun() ; err == nil {
+	//	t.Log(" 1== 1")
+	//}else{
+	//	t.Log(" 1 ++ 1")
+	//}
+
+}
---
+++ go_learning/src/ch5/loop/loop_test.go
@@ -0,0 +1,18 @@
+package loop
+
+import "testing"
+
+func TestWhileLoop(t *testing.T) {
+
+	n := 0
+	for n < 5 {
+		t.Log(n)
+		n++
+	}
+
+	for {
+		t.Log(n)
+		n++
+	}
+
+}
